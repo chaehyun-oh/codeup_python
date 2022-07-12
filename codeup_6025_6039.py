fe1ef90d8d46b59f1da3d1f649a4f11c630fe15c
chaehyun_oh <ohchaehh@gmail.com>
2022-07-12 21:30:39 +0900

    220712 codeup 문제풀이

---
+++ codeup_6025_6039.py
@@ -0,0 +1,51 @@
+#codeup_6025
+a, b = input().split()
+c = int(a) + int(b)
+print(c)
+
+# #codeup_6026
+a = input()
+b = input()
+c = float(a) + float(b)
+print(c)
+
+# #codeup_6032
+a = int(input())
+print(-a)
+
+# #codeup_6033
+n = ord(input())
+print(chr(n+1))
+
+#codeup_6034
+a, b = input().split()
+c = int(a) - int(b)
+print(c)
+
+#codeup_6035
+f1, f2 = input().split()
+m = float(f1) * float(f2)
+print(m)
+
+#codeup_6036
+w, n = input().split()
+print(w*int(n))
+
+#codeup_6037
+n = input()
+w = input()
+print(int(n)*w)
+
+#codeup_6038
+a, b = input().split()
+c = int(a)**int(b)
+print(c)
+
+#codeup_6039
+f1, f2 = input().split()
+c = float(f1)**float(f2)
+print(c)
+
+
+
+
